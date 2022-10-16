e69d2594d1e877059af1e56ce9b9d8e92dadb780
Project-S-31415 <projects31415.gif@gmail.com>
2022-10-16 11:44:18 +0900

    計算処理修正　UI微調整

--- RegularStarPolygonGenerator/Main.Designer.cs
+++ RegularStarPolygonGenerator/Main.Designer.cs
@@ -41,29 +41,29 @@
             this.Text3 = new System.Windows.Forms.Label();
             this.Num_ImgSize = new System.Windows.Forms.NumericUpDown();
             this.Text4 = new System.Windows.Forms.Label();
-            this.Num_LineLong = new System.Windows.Forms.NumericUpDown();
             ((System.ComponentModel.ISupportInitialize)(this.GeneratedImage)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.Num_N)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.Num_M)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.Num_ImgSize)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.Num_LineLong)).BeginInit();
             this.SuspendLayout();
             // 
             // Version
             // 
             this.Version.AutoSize = true;
+            this.Version.BackColor = System.Drawing.Color.Transparent;
             this.Version.Font = new System.Drawing.Font("MS UI Gothic", 10F);
-            this.Version.Location = new System.Drawing.Point(460, 85);
+            this.Version.Location = new System.Drawing.Point(456, 83);
             this.Version.Name = "Version";
-            this.Version.Size = new System.Drawing.Size(49, 17);
+            this.Version.Size = new System.Drawing.Size(41, 14);
             this.Version.TabIndex = 0;
             this.Version.Text = "v1.0.0";
             // 
             // Generation
             // 
-            this.Generation.Location = new System.Drawing.Point(186, 62);
+            this.Generation.Font = new System.Drawing.Font("MS UI Gothic", 12F);
+            this.Generation.Location = new System.Drawing.Point(450, 3);
             this.Generation.Name = "Generation";
-            this.Generation.Size = new System.Drawing.Size(65, 32);
+            this.Generation.Size = new System.Drawing.Size(47, 24);
             this.Generation.TabIndex = 2;
             this.Generation.Text = "生成";
             this.Generation.UseVisualStyleBackColor = true;
@@ -82,10 +82,10 @@
             // 
             // Num_N
             // 
-            this.Num_N.Font = new System.Drawing.Font("MS UI Gothic", 10F);
-            this.Num_N.Location = new System.Drawing.Point(64, 1);
+            this.Num_N.Font = new System.Drawing.Font("MS UI Gothic", 12F);
+            this.Num_N.Location = new System.Drawing.Point(67, 3);
             this.Num_N.Maximum = new decimal(new int[] {
-            9999,
+            99999,
             0,
             0,
             0});
@@ -95,7 +95,7 @@
             0,
             0});
             this.Num_N.Name = "Num_N";
-            this.Num_N.Size = new System.Drawing.Size(48, 24);
+            this.Num_N.Size = new System.Drawing.Size(60, 23);
             this.Num_N.TabIndex = 5;
             this.Num_N.Value = new decimal(new int[] {
             5,
@@ -105,20 +105,20 @@
             // 
             // Num_M
             // 
-            this.Num_M.Font = new System.Drawing.Font("MS UI Gothic", 10F);
-            this.Num_M.Location = new System.Drawing.Point(64, 19);
+            this.Num_M.Font = new System.Drawing.Font("MS UI Gothic", 12F);
+            this.Num_M.Location = new System.Drawing.Point(197, 3);
             this.Num_M.Maximum = new decimal(new int[] {
             9999,
             0,
             0,
             0});
             this.Num_M.Minimum = new decimal(new int[] {
-            2,
+            1,
             0,
             0,
             0});
             this.Num_M.Name = "Num_M";
-            this.Num_M.Size = new System.Drawing.Size(48, 24);
+            this.Num_M.Size = new System.Drawing.Size(60, 23);
             this.Num_M.TabIndex = 6;
             this.Num_M.Value = new decimal(new int[] {
             2,
@@ -129,29 +129,30 @@
             // Text1
             // 
             this.Text1.AutoSize = true;
-            this.Text1.Font = new System.Drawing.Font("MS UI Gothic", 10F);
-            this.Text1.Location = new System.Drawing.Point(2, 3);
+            this.Text1.Font = new System.Drawing.Font("MS UI Gothic", 12F);
+            this.Text1.Location = new System.Drawing.Point(4, 6);
             this.Text1.Name = "Text1";
-            this.Text1.Size = new System.Drawing.Size(68, 17);
+            this.Text1.Size = new System.Drawing.Size(63, 16);
             this.Text1.TabIndex = 7;
             this.Text1.Text = "辺の数n:";
             // 
             // Text2
             // 
             this.Text2.AutoSize = true;
-            this.Text2.Font = new System.Drawing.Font("MS UI Gothic", 10F);
-            this.Text2.Location = new System.Drawing.Point(2, 22);
+            this.Text2.Font = new System.Drawing.Font("MS UI Gothic", 12F);
+            this.Text2.Location = new System.Drawing.Point(131, 6);
             this.Text2.Name = "Text2";
-            this.Text2.Size = new System.Drawing.Size(71, 17);
+            this.Text2.Size = new System.Drawing.Size(67, 16);
             this.Text2.TabIndex = 8;
             this.Text2.Text = "飛び数m:";
             // 
             // Wiki
             // 
             this.Wiki.AutoSize = true;
-            this.Wiki.Location = new System.Drawing.Point(284, 4);
+            this.Wiki.Font = new System.Drawing.Font("MS UI Gothic", 12F);
+            this.Wiki.Location = new System.Drawing.Point(269, 80);
             this.Wiki.Name = "Wiki";
-            this.Wiki.Size = new System.Drawing.Size(271, 24);
+            this.Wiki.Size = new System.Drawing.Size(181, 16);
             this.Wiki.TabIndex = 9;
             this.Wiki.TabStop = true;
             this.Wiki.Text = "星型正多角形 - Wikipedia";
@@ -166,25 +167,25 @@
             this.Message.Location = new System.Drawing.Point(1, 101);
             this.Message.MaximumSize = new System.Drawing.Size(498, 498);
             this.Message.Name = "Message";
-            this.Message.Size = new System.Drawing.Size(0, 34);
+            this.Message.Size = new System.Drawing.Size(0, 27);
             this.Message.TabIndex = 10;
             // 
             // Text3
             // 
             this.Text3.AutoSize = true;
-            this.Text3.Font = new System.Drawing.Font("MS UI Gothic", 10F);
-            this.Text3.Location = new System.Drawing.Point(2, 41);
+            this.Text3.Font = new System.Drawing.Font("MS UI Gothic", 12F);
+            this.Text3.Location = new System.Drawing.Point(260, 6);
             this.Text3.Name = "Text3";
-            this.Text3.Size = new System.Drawing.Size(216, 34);
+            this.Text3.Size = new System.Drawing.Size(105, 16);
             this.Text3.TabIndex = 11;
-            this.Text3.Text = "生成に利用する画像サイズ(px)\r\n※≠出力サイズ:";
+            this.Text3.Text = "出力サイズ(px):";
             // 
             // Num_ImgSize
             // 
-            this.Num_ImgSize.Font = new System.Drawing.Font("MS UI Gothic", 10F);
-            this.Num_ImgSize.Location = new System.Drawing.Point(100, 56);
+            this.Num_ImgSize.Font = new System.Drawing.Font("MS UI Gothic", 12F);
+            this.Num_ImgSize.Location = new System.Drawing.Point(364, 3);
             this.Num_ImgSize.Maximum = new decimal(new int[] {
-            9999,
+            10000,
             0,
             0,
             0});
@@ -194,10 +195,10 @@
             0,
             0});
             this.Num_ImgSize.Name = "Num_ImgSize";
-            this.Num_ImgSize.Size = new System.Drawing.Size(56, 24);
+            this.Num_ImgSize.Size = new System.Drawing.Size(60, 23);
             this.Num_ImgSize.TabIndex = 12;
             this.Num_ImgSize.Value = new decimal(new int[] {
-            1000,
+            10000,
             0,
             0,
             0});
@@ -205,42 +206,19 @@
             // Text4
             // 
             this.Text4.AutoSize = true;
-            this.Text4.Font = new System.Drawing.Font("MS UI Gothic", 10F);
-            this.Text4.Location = new System.Drawing.Point(2, 81);
+            this.Text4.Font = new System.Drawing.Font("MS UI Gothic", 12F);
+            this.Text4.Location = new System.Drawing.Point(1, 80);
             this.Text4.Name = "Text4";
-            this.Text4.Size = new System.Drawing.Size(114, 17);
+            this.Text4.Size = new System.Drawing.Size(261, 16);
             this.Text4.TabIndex = 13;
-            this.Text4.Text = "一辺の長さ(px):";
-            // 
-            // Num_LineLong
-            // 
-            this.Num_LineLong.Font = new System.Drawing.Font("MS UI Gothic", 10F);
-            this.Num_LineLong.Location = new System.Drawing.Point(93, 78);
-            this.Num_LineLong.Maximum = new decimal(new int[] {
-            9999,
-            0,
-            0,
-            0});
-            this.Num_LineLong.Minimum = new decimal(new int[] {
-            1,
-            0,
-            0,
-            0});
-            this.Num_LineLong.Name = "Num_LineLong";
-            this.Num_LineLong.Size = new System.Drawing.Size(51, 24);
-            this.Num_LineLong.TabIndex = 14;
-            this.Num_LineLong.Value = new decimal(new int[] {
-            100,
-            0,
-            0,
-            0});
+            this.Text4.Text = "コンソールに詳しい情報が表示されます。";
             // 
             // RegularStarPolygonGenerator
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 23F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 19F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(500, 600);
-            this.Controls.Add(this.Num_LineLong);
+            this.Controls.Add(this.Version);
             this.Controls.Add(this.Text4);
             this.Controls.Add(this.Generation);
             this.Controls.Add(this.Num_ImgSize);
@@ -252,19 +230,20 @@
             this.Controls.Add(this.Text2);
             this.Controls.Add(this.Text1);
             this.Controls.Add(this.GeneratedImage);
-            this.Controls.Add(this.Version);
             this.Font = new System.Drawing.Font("MS UI Gothic", 14F);
             this.HelpButton = true;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximumSize = new System.Drawing.Size(516, 639);
+            this.MinimumSize = new System.Drawing.Size(516, 639);
             this.Name = "RegularStarPolygonGenerator";
-            this.Text = "Form1";
+            this.Text = "RegularStarPolygonGenerator";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.RegularStarPolygonGenerator_FormClosed);
             this.Load += new System.EventHandler(this.RegularStarPolygonGenerator_Load);
             ((System.ComponentModel.ISupportInitialize)(this.GeneratedImage)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.Num_N)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.Num_M)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.Num_ImgSize)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.Num_LineLong)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -284,7 +263,6 @@
         private System.Windows.Forms.Label Text3;
         private System.Windows.Forms.NumericUpDown Num_ImgSize;
         private System.Windows.Forms.Label Text4;
-        private System.Windows.Forms.NumericUpDown Num_LineLong;
     }
 }
 
--- RegularStarPolygonGenerator/Main.cs
+++ RegularStarPolygonGenerator/Main.cs
@@ -1,11 +1,11 @@
-﻿using RegularStarPolygonGenerator.Properties;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Windows.Forms;
 
-//正n角形:https://www.umayadia.com/cssample/sample0201/Sample279WinFormDrawPolygon.htm#A5
 namespace RegularStarPolygonGenerator
 {
     public partial class RegularStarPolygonGenerator : Form
@@ -25,49 +25,73 @@ namespace RegularStarPolygonGenerator
             try
             {
                 Console.WriteLine($"{DateTime.Now:HH:mm:ss.ffff} 生成開始");
+                DateTime StartTime = DateTime.Now;
                 Message.Text = "生成中...";
-
                 int N = (int)Num_N.Value;
                 int M = (int)Num_M.Value;
                 int ImgSize = (int)Num_ImgSize.Value;
-                int Linelong = (int)Num_LineLong.Value;
                 if (N <= M * 2)
                     throw new Exception("値が不正です。");
                 if (N % M == 0)
-                    Console.WriteLine($"星型正多角形ではありません。");
-                Draw();
-
-
-
-
-
-
-            }
-            catch (OutOfMemoryException ex)
-            {
-                Message.Text = "メモリが不足しています。画像サイズを小さくしてください。";
+                    Console.WriteLine("※星型正多角形ではありません。");
+                Bitmap Img = Draw(N, M, ImgSize);
+                Console.WriteLine($"{DateTime.Now:HH:mm:ss.ffff} 生成完了");
+                Console.WriteLine($"生成時間:{(DateTime.Now - StartTime).TotalMilliseconds}ms");
+                if (!Directory.Exists("output"))
+                    Directory.CreateDirectory("output");
+                if (!Directory.Exists($"output\\{N}"))
+                    Directory.CreateDirectory($"output\\{N}");
+                if (!Directory.Exists($"output\\{N}\\{M}"))
+                    Directory.CreateDirectory($"output\\{N}\\{M}");
+                Img.Save($"output\\{N}\\{M}\\{M}-{N}-{ImgSize}px.png", ImageFormat.Png);
+                Console.WriteLine($"{DateTime.Now:HH:mm:ss.ffff} 出力完了");
+                if (N % M == 0)
+                    Message.Text = "※星型正多角形ではありません。";
             }
             catch (Exception ex)
             {
                 Message.Text = ex.Message;
-
-
-
+                Console.WriteLine("////////////////////////////////////////////////////////////////////////////////////////////////////\n" + ex + "\n////////////////////////////////////////////////////////////////////////////////////////////////////");
             }
-
-
-
+            finally
+            {
+                Console.WriteLine("\n----------------------------------------------------------------------------------------------------\n");
+            }
         }
-        public void Draw()
+        public Bitmap Draw(int N, int M, int ImgSize)
         {
+            Console.WriteLine($"辺の数:{N}");
+            Console.WriteLine($"飛び数:{M}");
+            Console.WriteLine($"画像サイズ:{ImgSize}px");
+            int L = ImgSize / 2;//半径
+            Console.WriteLine($"外接円半径:{L}px");
+            Bitmap Img = new Bitmap(ImgSize, ImgSize);
+                Graphics g = Graphics.FromImage(Img);
+            double AngleRad = (Math.PI * 2) / N;
+            Console.WriteLine($"外接正n角形ラジアン:{AngleRad}");
 
-            int N = (int)Num_N.Value;
-            int M = (int)Num_M.Value;
-            int ImgSize = (int)Num_ImgSize.Value;
-            int Linelong = (int)Num_LineLong.Value;
-            Bitmap Img = new Bitmap((int)Num_ImgSize.Value, (int)Num_ImgSize.Value);
-            Graphics g = Graphics.FromImage(Img);
 
+            List<Point> Points_RegularPolygon = new List<Point>();//正n角形の頂点座標(中心:0,0)
+            List<Point> Points_StarRegularPolygon = new List<Point>();//星型正n角形の頂点座標
+            for (int i = 0; i < N; i++)
+            {
+                double AngleRad_ = AngleRad * i + Math.PI;
+                Points_RegularPolygon.Add(new Point((int)Math.Round(Math.Sin(AngleRad_) * L, MidpointRounding.AwayFromZero), (int)Math.Round(Math.Cos(AngleRad_) * L, MidpointRounding.AwayFromZero)));
+                Console.WriteLine($"外接正n角形頂点座標{i + 1}:{Points_RegularPolygon[i].X + L},{Points_RegularPolygon[i].Y + L}");
+            }
+            Console.WriteLine($"{DateTime.Now:HH:mm:ss.ffff} 正n角形頂点座標計算終了");
+            int p = -M;
+            for (int i = 0; i < Points_RegularPolygon.Count; i++)
+            {
+                p += M;
+                if (p >= N)
+                    p -= N;
+                Points_StarRegularPolygon.Add(new Point(Points_RegularPolygon[p].X, Points_RegularPolygon[p].Y));
+                Console.WriteLine($"星型正n角形頂点座標{i + 1}:外接正n角形頂点{p + 1},{Points_StarRegularPolygon[p + 1].X + L},{Points_StarRegularPolygon[p+1].Y + L}");
+            }
+            Console.WriteLine($"{DateTime.Now:HH:mm:ss.ffff} 星型正n角形頂点座標計算終了");
+            g.TranslateTransform(L, L); //移動して-部分を無くす
+            /*旧版
             int Angle = 180 * ((N - 2) * M / N) / N;
             double AngleRad = Angle * (Math.PI / 180);//Math.SinCosTanはこれ使う
             List<Point> Points = new List<Point>
@@ -92,14 +116,10 @@ namespace RegularStarPolygonGenerator
                 double PY = PointY_ + Linelong * Math.Cos(AngleRad);
                 double PX_ = PointX_ - Linelong * Math.Sin(AngleRad);
                 double PY_ = PointY_ - Linelong * Math.Cos(AngleRad);
-                
+
                 Console.WriteLine($"移動後座標X:{PX} Y:{PY} 逆X:{PX_} 逆Y:{PY_} 半径二乗:{L * L}");
                 Console.WriteLine($"そのまま:{(OX - PX) * (OX - PX) + (OY - PY) * (OY - PY)} X変:{(OX - PX_) * (OX - PX_) + (OY - PY) * (OY - PY)} Y変:{(OX - PX) * (OX - PX) + (OY - PY_) * (OY - PY_)} XY変:{(OX - PX_) * (OX - PX_) + (OY - PY_) * (OY - PY_)}");
-              
-
-
-
-                //↓
+                //↓？
                 if ((OX - PX) * (OX - PX) + (OY - PY) * (OY - PY) <= L * L)//新しい点が円の中にあるか
                 {
                     Console.WriteLine("そのまま");
@@ -130,18 +150,20 @@ namespace RegularStarPolygonGenerator
                 Points.Add(new Point(PointX_ + 100, PointY_ + 100));
             }
             Points.Add(new Point(100, 100));
-
+            */
             g.Clear(Color.White);
-            g.DrawPolygon(Pens.Black, Points.ToArray());
-            Img.Save("test.png", System.Drawing.Imaging.ImageFormat.Png);
-
-            Img.Dispose();
-
+            g.DrawPolygon(Pens.Black, Points_StarRegularPolygon.ToArray());
+            return Img;
         }
 
         private void Wiki_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
             Process.Start("https://ja.wikipedia.org/wiki/%E6%98%9F%E5%9E%8B%E6%AD%A3%E5%A4%9A%E8%A7%92%E5%BD%A2");
+        }
+
+        private void RegularStarPolygonGenerator_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Application.Exit();
         }
     }
 }
